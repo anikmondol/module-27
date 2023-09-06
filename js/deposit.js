c439e84c8ac5a2a75ab5953f7a418271e7a64020
anikmondol <anikmondol558363@gmil.com>
2023-09-07 00:40:29 +0600

    added test

--- js/deposit.js
+++ js/deposit.js
@@ -1,1 +1,36 @@
-console.log('tamer name ki')
+/*
+1. add event listener to deposit button
+2. get deposit amount from the deposit input field
+2-5. convert string deposit amount to a number type 
+3.clear the deposit input field after getting the value
+4. get the previous deposit total
+5. calculate new deposit total and set the value to the deposit total
+6. get current balance total
+7. calculate the new balance and set it to balance total element
+*/ 
+
+// step-1
+document.getElementById('btn-deposit').addEventListener('click',function(){
+    // step-2
+   const depositField = document.getElementById('deposit-field');
+   const newDepositAmount = parseFloat(depositField.value);
+    // step-3
+   depositField.value = '';
+    // step-4
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotal = parseFloat(depositTotalElement.innerText)
+
+    // step-5
+    const newDepositTotal = newDepositAmount + previousDepositTotal;
+    depositTotalElement.innerText = newDepositTotal;
+
+    // step-6
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalAmount = parseFloat(balanceTotalElement.innerText);
+    
+    // step-7
+    const newBalanceTotal = previousBalanceTotalAmount + newDepositTotal;
+    
+    balanceTotalElement.innerText = newBalanceTotal;
+    
+})
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,1 +1,35 @@
-console.log('amer name hoicay anik')
+/*
+1. add event handler with the withdraw button
+2. get the withdraw amount
+3. clear the withdraw input field
+4. get previous withdraw total
+5. calculate total withdraw amount and set it to the withdraw total element
+6. get previous balance
+7. calculate new balance and set it to the balance total element
+*/ 
+
+
+// step-1
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    // step-2
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmount = parseFloat(withdrawField.value);
+    // step-3
+    withdrawField.value = '';
+  
+    // step-4
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithTotal = parseFloat(withdrawTotalElement.innerText);
+
+    // step-5
+    const newWithdrawTotal = newWithdrawAmount + previousWithTotal;
+    withdrawTotalElement.innerText = newWithdrawTotal;
+
+    // step-6
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
+
+    // step-7
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+})
